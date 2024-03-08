54ea4af845ffd223a6ad0275f42542ca9f6e8b24
emd22 <e@ethanm.ca>
2024-03-08 19:00:21 -0400

    fixed formatting issues, added text sanitization and fixed sql generation

--- dbgen.py
+++ dbgen.py
@@ -2,6 +2,16 @@ import csv
 from typing import List, Final, Optional
 import re
 
+def sanitize(data: str) -> str:
+    # apostrophes can be a catastrophe
+    if '\'' in data:
+        data = data.replace('\'', '')
+    # replace any non-ascii characters
+    data = ''.join(ch for ch in data if ord(ch) < 128).strip()
+    if len(data) > 0 and data[-1] == ',':
+        data = data[:-1]
+    return data
+
 class CommonName:
     def __init__(self, id: str, name: str):
         self.id = id
@@ -16,7 +26,7 @@ class NameDB:
     def load(self, name_file):
         name_reader = csv.DictReader(name_file)
         for row in name_reader:
-            name_obj = CommonName(row['taxonID'], row['vernacularName'])
+            name_obj = CommonName(row['taxonID'], sanitize(row['vernacularName']))
             self.common_names.append(name_obj)
 
     def print(self, count: Optional[int]=None):
@@ -36,20 +46,20 @@ name_db = NameDB()
 
 class Author:
     def __init__(self, author_name: str):
-        self.author_name = author_name
+        self.author_name = sanitize(author_name)
     def __str__(self) -> str:
         return self.author_name
 
 class Family:
     def __init__(self, taxon_id: str, scientific_name: str):
         self.taxon_id = taxon_id
-        self.scientific_name: str = scientific_name
+        self.scientific_name: str = sanitize(scientific_name)
         self.common_name: Optional[str] = name_db.find(taxon_id)
 
 class Genus:
     def __init__(self, taxon_id: str, scientific_name: str, year_discovered: int, family: Family, authors: List[str]):
         self.taxon_id = taxon_id
-        self.scientific_name = scientific_name
+        self.scientific_name = sanitize(scientific_name)
         self.year_discovered = year_discovered
         self.authors: List[str] = authors
         self.family = family
@@ -60,7 +70,7 @@ class Genus:
 class Species:
     def __init__(self, taxon_id: str, scientific_name: str, year_discovered: Optional[int], genus: Genus, authors: List[str]):
         self.taxon_id = taxon_id
-        self.scientific_name = scientific_name
+        self.scientific_name = sanitize(scientific_name)
         self.common_name = name_db.find(taxon_id)
         self.year_discovered = year_discovered
         self.authors: List[str] = authors
@@ -109,9 +119,11 @@ class BirdDB:
 
         prev_name = ''
         for name in splits:
+            name = sanitize(name)
+
             # most likely someones initials after their surname.
             # no idea why they would separate with a comma,  but here we are.
-            if len(name) <= 3:
+            if len(name) <= 3 and len(splits) > 2:
                 if len(authors) > 0:
                     authors.pop()
                 authors.append(name.strip() + ', ' + prev_name.strip())
@@ -121,6 +133,20 @@ class BirdDB:
 
             prev_name = name
         return authors
+
+    def add_unique_authors(self, author_names: List[str]):
+        authors_to_add = author_names.copy()
+        authors: List[Author] = []
+
+        for author in self.authors:
+            if author.author_name in authors_to_add:
+                authors_to_add.remove(author.author_name)
+                continue
+
+        for name in authors_to_add:
+
+            author = Author(name)
+            self.authors.append(author)
 
     def eval_row(self, row):
         rank: str = row['taxonRank']
@@ -132,20 +158,12 @@ class BirdDB:
         authorship_string = row['scientificNameAuthorship']
         id: str = row['taxonID']
 
+        author_names: List[str] = []
+
         # this is SUPER slow, best to only run this when we need to.
         if rank == 'genus' or rank == 'species':
-            author_names: List[str] = self.get_author_names(authorship_string)
-            authors_to_add = author_names.copy()
-            authors: List[Author] = []
-
-            for author in self.authors:
-                if author.author_name in authors_to_add:
-                    authors_to_add.remove(author.author_name)
-                    continue
-
-            for name in author_names:
-                author = Author(name)
-                self.authors.append(author)
+            author_names = self.get_author_names(authorship_string)
+            self.add_unique_authors(author_names.copy())
 
         if rank == 'family':
             family = Family(id, scientific_name)
@@ -190,7 +208,7 @@ class SQLGenerator:
         self.output_file = open(output_filename, "w")
 
     def generate_authors(self):
-        output_data = "INSERT INTO authors VALUES\n"
+        output_data = "INSERT INTO authors VALUES \\\n"
 
 
         for author in self.bird_db.authors:
@@ -203,7 +221,7 @@ class SQLGenerator:
         self.output_file.write(output_data)
 
     def generate_families(self):
-        output_data = "INSERT INTO families VALUES\n"
+        output_data = "INSERT INTO families VALUES \\\n"
 
         for family in self.bird_db.families:
             output_data += f"\t('{family.scientific_name}', '{family.common_name}')"
@@ -215,7 +233,7 @@ class SQLGenerator:
         self.output_file.write(output_data)
 
     def generate_genera(self):
-        output_data = "INSERT INTO genera VALUES\n"
+        output_data = "INSERT INTO genera VALUES \\\n"
 
         for genus in self.bird_db.genera:
             output_data += f"\t('{genus.scientific_name}', {genus.year_discovered}, (SELECT scientific_name FROM families WHERE scientific_name = '{genus.family.scientific_name}'))"
@@ -227,7 +245,7 @@ class SQLGenerator:
         self.output_file.write(output_data)
 
     def generate_species(self):
-        output_data = "INSERT INTO species VALUES\n"
+        output_data = "INSERT INTO species VALUES \\\n"
 
         for species in self.bird_db.species:
             output_data += f"\t('{species.scientific_name}', '{species.common_name}', {species.year_discovered}, (SELECT scientific_name FROM genera WHERE scientific_name = '{species.genus.scientific_name}'))"
@@ -239,11 +257,11 @@ class SQLGenerator:
         self.output_file.write(output_data)
 
     def generate_discovery_types(self):
-        output_data = "INSERT INTO discovery_types\nVALUES\n\t('SPECIES')\n\t('GENUS');\n\n"
+        output_data = "INSERT INTO discovery_types VALUES\\\n\t('SPECIES'),\n\t('GENUS');\n\n"
         self.output_file.write(output_data)
 
     def generate_bird_authors(self):
-        output_data = "INSERT INTO bird_authors VALUES\n"
+        output_data = "INSERT INTO bird_authors VALUES \\\n"
 
         # shield your eyes, this one is pretty ripe.
         for genus in self.bird_db.genera:
